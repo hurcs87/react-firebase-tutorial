992cdf216558ade8bdf411a911e7ed7cb2751da3
hurcs87 <[zoe.hurcombe@holidayextras.com]>
2019-03-21 14:45:13 +0000

    fixed auth issues

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,13 +1,14 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import keys from './keys'
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: keys.API_KEY,
+  authDomain: keys.AUTH_DOMAIN,
+  databaseURL: keys.DATABASE_URL,
+  projectId: keys.PROJECT_ID,
+  storageBucket: keys.STORAGE_BUCKET,
+  messagingSenderId: keys.MESSAGING_SENDER_ID,
 };
 
 class Firebase {
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SingUp = () => (
+import * as ROUTES from '../../constants/routes';
+
+const SingUpPage = () => (
   <div>
     <h1>SingUp</h1>
   </div>
 );
 
-export default SingUp;
+const INTIAL_STATE = {
+  username: '',
+  email: '',
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INTIAL_STATE };
+  }
+
+  onSubmit = event => {
+
+  }
+
+  onChange = event => {
+
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+
+      </form>
+    )
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Dont have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+export default SingUpPage;
+
+export { SignUpForm, SignUpLink };
